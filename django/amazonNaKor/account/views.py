f48486f06771268ba404a58746813b5a9b4355d7
Jihoon Kim <jihoon_kim@apple.com>
2019-01-05 16:14:42 -0800

    Added the navbar support

--- django/amazonNaKor/account/views.py
+++ django/amazonNaKor/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegistrationForm, EditProfileForm
 
@@ -18,10 +19,12 @@ def register(request):
         args = {'form': form}
         return render(request, 'account/reg_form.html', args)
 
+@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'account/profile.html', args)
 
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -33,7 +36,8 @@ def edit_profile(request):
         form = EditProfileForm(instance=request.user)
         args = {'form': form}
         return render(request, 'account/edit_profile.html', args)
-
+        
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
@@ -49,5 +53,6 @@ def change_password(request):
         args = {'form': form}
         return render(request, 'account/change_password.html', args)
 
+@login_required
 def home(request):
     return render(request, 'account/home.html')
