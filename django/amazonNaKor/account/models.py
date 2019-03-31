8cfdc04ffb818d00138785d532dec005aa25438b
Jihoon Kim <jihoonkim@Jihoons-MBP15.local>
2019-03-31 00:13:42 -0700

    Some fixes

--- django/amazonNaKor/account/models.py
+++ django/amazonNaKor/account/models.py
@@ -62,7 +62,7 @@ class Recepient(models.Model):
     phone = models.IntegerField("전화번호", default='')
     postal_code = models.IntegerField("우편번호", default='')
     address = models.CharField("주소", max_length=1024, default='')
-    customs_id = models.CharField("통관고유호", max_length=1024, default='')
+    customs_id = models.CharField("통관고유부호", max_length=1024, default='')
 
 class Package(models.Model):
     sender_email = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='sender_email')
@@ -114,4 +114,6 @@ class Item(models.Model):
     price = models.FloatField("단가 (USD)", default='0.0')
     qty = models.IntegerField("수량", default='1')
     delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)    # One (item) to Many (Delivery) relationship
-
+    # TODO: 
+    # 상품코드
+    # HS코드
